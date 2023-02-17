eacd388267c2194f1746129c5dce23f63869530c
Сергей Субботин <zarodiny@yandex.ru>
2023-02-17 18:48:27 +1000

    добавляет view

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
+import { render } from './render.js';
+import ButtonShowMoreView from './view/show-more-button-view.js';
+import SortView from './view/sort-view.js';
 
+const siteMain = document.querySelector('.main');
+
+render(new SortView(), siteMain);
+render(new ButtonShowMoreView(), siteMain);
